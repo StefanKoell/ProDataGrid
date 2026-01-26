cde30fe8dc9f92a1e2299ebbab677194280e76e1
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2026-01-26 16:21:58 +0100

    Allow AssertionException in ScrollColumnIntoView test

--- src/Avalonia.Controls.DataGrid.UnitTests/Columns/DataGridColumnsBranchCoverageTests.cs
+++ src/Avalonia.Controls.DataGrid.UnitTests/Columns/DataGridColumnsBranchCoverageTests.cs
@@ -859,7 +859,7 @@ public class DataGridColumnsBranchCoverageTests
             {
                 ex = tie.InnerException;
             }
-            Assert.Contains(ex.GetType().Name, new[] { "DebugAssertException", nameof(ArgumentOutOfRangeException) });
+            Assert.Contains(ex.GetType().Name, new[] { "DebugAssertException", "AssertionException", nameof(ArgumentOutOfRangeException) });
         }
         finally
         {
